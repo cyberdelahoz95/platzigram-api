12516a5b9c3bda9cf85dd6cfbef6b4d9e67b85ec
Henry de la Hoz <cyberdelahoz95@gmail.com>
2017-06-23 16:56:46 -0500

    adding stub object for faking a db object, adding a config object for setting values during deployment, adding env variable for flaggin test enviroment

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -4,7 +4,7 @@ export default {
       id: '02b353bf5358995bc7d193ed1ce9c2eaec2b694b21d2f96232c9d6a0832121d1',
       publicId: 'foo123',
       userId: 'platzigram',
-      src: 'http://platzigram.test/img.jpg',
+      src: 'http://platzigram.test/foo123.jpg',
       description: '#awesome',
       tags: ['awesome'],
       createdAt: new Date().toString()
--- test/pictures-test.js
+++ test/pictures-test.js
@@ -2,20 +2,47 @@
 
 import micro from 'micro'
 import test from 'ava'
-import uuid from 'uuid-base62'
 import listen from 'test-listen' // testear microservicios
 import request from 'request-promise' // modulo para hacer http request haciendo promesas, cómo por ejemplo con fetch api
 import pictures from '../pictures'
 import fixtures from './fixtures'
 
+test.beforeEach(async t => {
+  let srv = micro(pictures)
+  t.context.url = await listen(srv)
+})
+
 test('GET /:id', async t => {
   let image = fixtures.getImage()
-  let srv = micro(pictures)
-  let url = await listen(srv)
+
+  let url = t.context.url
 
   let body = await request({ uri: `${url}/${image.publicId}`, json: true })
   t.deepEqual(body, image)
 })
 
-test.todo('POST /')
+test('POST /', async t =>  {
+  let image = fixtures.getImage()
+  let url = t.context.url
+
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body:{
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    resolveWithFullResponse: true
+  }
+
+  let response = await request(options)
+
+  //el atributo resolveWithFullResponse nos permite tener acceso a todo el objeto de respuesta, ya q por defecto requesto promise solo devuelve el atributo body
+
+  t.is(response.statusCode, 201)
+  t.deepEqual(response.body, image)
+})
+
 test.todo('POST /:id/like')
